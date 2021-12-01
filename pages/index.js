a3f5a183359f26f07453064d1e4692dca8e163e0
jordanteague <ejteague@gmail.com>
2021-11-30 21:19:18 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { Component, useEffect, useState } from "react";
 import Web3 from "web3";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { Box, Flex, Button, Text, Spacer, VStack, HStack } from "@chakra-ui/react";
+import { Box, Flex, Button, Input, Text, Spacer, VStack, HStack } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 
 class App extends Component {
@@ -41,21 +41,35 @@ class App extends Component {
 
   }
 
-  send = async() => {
+  send = async(e) => {
+
+    e.preventDefault();
+    let object = event.target;
+    var array = [];
+    for (let i = 0; i < object.length; i++) {
+      array[object[i].name] = object[i].value;
+    }
+
+    var { recipient, amount } = array;
 
     const { web3, account, chainId } = this.state;
 
-    await web3.eth.sendTransaction({from: account, to: '0x348E75571db50E4572871eaBC0Fa4ECF5f6DEe30', value: 100});
+    await web3.eth.sendTransaction({from: account, to: array['to'], value: array['amount']});
   }
 
   render() {
 
     return (
       <Layout>
-        <Text>account: {this.state.account}</Text>
-        <Text>chain: {this.state.chainId}</Text>
+        <Button>{this.state.account!=null ? this.state.account : <>Connect Wallet</>}</Button>
+        {this.state.chainId!=null ? <Button>chain: {this.state.chainId}</Button> : ''}
+        <form onSubmit={this.send}>
+          <Text>To</Text><Input name="to" />
+          <Text>Amount</Text><Input name="amount" />
+          <Button type="submit">Send</Button>
+        </form>
 
-        <Button onClick={this.send}>Test Sign</Button>
+
       </Layout>
     );
 }
