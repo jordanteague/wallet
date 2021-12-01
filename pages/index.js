30ba8d4e6b783c237f684eac69553a564fd2cf1f
jordanteague <ejteague@gmail.com>
2021-11-30 20:26:58 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,17 @@
-import React, { Component, useEffect } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Web3 from "web3";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { Box, Flex, Button, Text, Spacer, VStack, HStack } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 
-class Home extends Component {
+class App extends Component {
 
   state = {
-    fetching: false,
-    address: "",
-    web3: null,
-    provider: null,
-    connected: false,
-    chainId: 1,
-    networkId: 1,
-    assets: [],
-    showModal: false,
-    pendingRequest: false,
-    result: null
-  };
-
-  constructor(props) {
-    super(props);
+    web3: null
   }
 
-  componentDidMount = async() => {
+  async componentDidMount() {
 
     const providerOptions = {
       walletconnect: {
@@ -44,77 +30,29 @@ class Home extends Component {
 
     const web3 = new Web3(provider);
 
-    console.log(web3);
-
-    this.setState({ web3, provider })
+    this.setState({ web3 });
 
   }
 
-  toggleModal = () => {
-    this.setState({ showModal: !this.state.showModal });
-  }
-
-  testSignMessage = async () => {
+  send = async() => {
 
     const { web3 } = this.state;
 
     let accounts = await web3.eth.getAccounts();
-    let address = accounts[0];
-
-    if (!web3) {
-      return;
-    }
-
-    // test message
-    const message = "My email is john@doe.com - 1537836206101";
-
-    // hash message
-    //const hash = hashPersonalMessage(message);
-    const hash = "fakehash";
-
-    try {
-      // open modal
-      this.toggleModal();
-
-      // toggle pending request indicator
-      this.setState({ pendingRequest: true });
-
-      // send message
-      const result = await web3.eth.sign(hash, address);
-
-      // verify signature
-      const signer = recoverPublicKey(result, hash);
-      const verified = signer.toLowerCase() === address.toLowerCase();
-
-      // format displayed result
-      const formattedResult = {
-        action: ETH_SIGN,
-        address,
-        signer,
-        verified,
-        result
-      };
-
-      // display result
-      this.setState({
-        web3,
-        pendingRequest: false,
-        result: formattedResult || null
-      });
-    } catch (error) {
-      console.error(error); // tslint:disable-line
-      this.setState({ web3, pendingRequest: false, result: null });
-    }
-  };
+    let account = accounts[0];
+    await web3.eth.sendTransaction({from: account, to: '0x348E75571db50E4572871eaBC0Fa4ECF5f6DEe30', value: 100});
+  }
 
   render() {
+
     return (
       <Layout>
-        <Button onClick={this.testSend}>Test Send</Button>
-        <Button onClick={this.testSignMessage}>Test Sign</Button>
+        <>Connected:</>
+
+        <Button onClick={this.send}>Test Sign</Button>
       </Layout>
     );
-  }
+}
 }
 
-export default Home;
+export default App;
